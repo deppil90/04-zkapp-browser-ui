310ebf693af7005e59624b83330ff3ddd916ee39
depil <aduhsaja8@gmail.com>
2022-12-14 14:52:29 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-8df5e34ae69315ab.js
+++ _next/static/chunks/webpack-8df5e34ae69315ab.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "082bee86e6984f43" + ".css";
+/******/ 			return "static/css/" + "33ea281b1ac13caa" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
